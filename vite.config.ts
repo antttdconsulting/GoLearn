4dca614131289949bb39eddacf1f2d9fa7584b9e
antttdconsulting <antt.tdconsulting@gmail.com>
2025-09-25 15:09:38 +0700

    ddd

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@/components': path.resolve(__dirname, './src/shared/components'),
-      '@/ui': path.resolve(__dirname, './src/shared/ui'),
-      '@/hooks': path.resolve(__dirname, './src/hooks'),
-      '@/lib': path.resolve(__dirname, './src/lib'),
-      '@/types': path.resolve(__dirname, './src/types'),
-      '@/utils': path.resolve(__dirname, './src/utils'),
-      '@/assets': path.resolve(__dirname, './src/assets'),
-      '@/features': path.resolve(__dirname, './src/features'),
-      '@/shared': path.resolve(__dirname, './src/shared'),
-      '@/app': path.resolve(__dirname, './src/app'),
+      "@": path.resolve(__dirname, "./src"),
+      "@/components": path.resolve(__dirname, "./src/shared/components"),
+      "@/ui": path.resolve(__dirname, "./src/shared/ui"),
+      "@/hooks": path.resolve(__dirname, "./src/hooks"),
+      "@/lib": path.resolve(__dirname, "./src/lib"),
+      "@/types": path.resolve(__dirname, "./src/types"),
+      "@/utils": path.resolve(__dirname, "./src/utils"),
+      "@/assets": path.resolve(__dirname, "./src/assets"),
+      "@/features": path.resolve(__dirname, "./src/features"),
+      "@/shared": path.resolve(__dirname, "./src/shared"),
+      "@/app": path.resolve(__dirname, "./src/app"),
     },
   },
   server: {
-    port: 5634,
+    port: 5636,
     open: true,
     headers: {
-      'Cross-Origin-Embedder-Policy': 'cross-origin',
-      'Cross-Origin-Opener-Policy': 'same-origin',
+      "Cross-Origin-Embedder-Policy": "cross-origin",
+      "Cross-Origin-Opener-Policy": "same-origin",
     },
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "nonfacultative-nontrained-lore.ngrok-free.dev", // 👈 thêm host ngrok ở đây
+    ],
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true,
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
           if (!assetInfo.name) return `assets/[name]-[hash][extname]`;
-          if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)) {
+          if (
+            /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)
+          ) {
             return `assets/videos/[name]-[hash][extname]`;
           }
           return `assets/[name]-[hash][extname]`;
@@ -43,4 +50,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
